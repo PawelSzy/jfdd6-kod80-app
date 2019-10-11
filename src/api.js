a7d595119baf6de9059128558ea4b7f17ac5af09
PawelSzy <peal1982@gmail.com>
2019-10-11 19:01:47 +0200

    Updates

--- src/api.js
+++
@@ -1,7 +0,0 @@
-let url = 'http://localhost:3010/api'
-
-if (process.env.NODE_ENV === "production") {
-  url = 'https://radiant-mountain-66074.herokuapp.com/api/'
-}
-
-export default { url }
--- src/date/sort-concert-by-date.js
+++
@@ -1,4 +0,0 @@
-import convertDate from './convert-date'
-
-
-export default (concert1, concert2) => convertDate(concert1.date) - convertDate(concert2.date)
